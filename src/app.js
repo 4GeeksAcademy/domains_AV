cde8f76ca1a977d872a7b853f9b6c398fde60f54
Ana VR <52681114+Anne1916@users.noreply.github.com>
2025-07-10 22:03:00 +0000

    get domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.us', '.io']
+
+let pronoun2 = Math.floor(Math.random() * pronoun.length)
+let adj2 = Math.floor(Math.random() * adj.length)
+let noun2 = Math.floor(Math.random() * noun.length)
+let extension2 = Math.floor(Math.random() * extension.length)  
+
+
+console.log(`${pronoun[pronoun2]}${adj[adj2]}${noun[noun2]}${extension[extension2]}`)
 };
