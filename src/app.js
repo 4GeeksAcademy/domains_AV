d95fef5088339fb73c352557ec2fbb164fff8531
Ana VR <52681114+Anne1916@users.noreply.github.com>
2025-07-11 16:01:55 +0000

    All domains generated

--- src/app.js
+++ src/app.js
@@ -5,18 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let extension = ['.com', '.net', '.us', '.io']
-
-let pronoun2 = Math.floor(Math.random() * pronoun.length)
-let adj2 = Math.floor(Math.random() * adj.length)
-let noun2 = Math.floor(Math.random() * noun.length)
-let extension2 = Math.floor(Math.random() * extension.length)  
-
-
-console.log(`${pronoun[pronoun2]}${adj[adj2]}${noun[noun2]}${extension[extension2]}`)
-};
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+  
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extension) {
+          console.log(`${p}${a}${n}${e}`)
+        }
+      }
+    }
+  }
+}
